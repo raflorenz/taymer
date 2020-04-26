152ee8ee78e0c12d3bb5c85fa0d7c9c6c4944523
Raffy Lorenzana <rjlorenz931@gmail.com>
2020-04-27 04:44:58 +0800

    implement count up timer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [seconds, setSeconds] = useState(0);
+  const [active, setActive] = useState(false);
+
+  const pad = val => val > 9 ? val : '0' + val;
+
+  useEffect(() => {
+    let interval = null;
+
+    if (active) {
+      interval = setInterval(() => setSeconds(seconds => seconds + 1), 1000);
+    }
+
+    return () => clearInterval(interval);
+  }, [active]);
+
   return (
     <div className="app">
       <h1>Taymer</h1>
+      <div className="task-item">
+        <p className="task-title">Drink Coffee <span>{pad(Math.floor(seconds / 60 % 60))}m:{pad(seconds % 60)}s</span></p>
+        <button onClick={() => setActive(!active)}>start</button>
+      </div>
     </div>
   );
 }
