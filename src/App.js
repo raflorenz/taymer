8cb12e0c7f75cd24eba4ab2b66e1c49456b5af40
Raffy Lorenzana <rjlorenz931@gmail.com>
2020-05-01 23:57:57 +0800

    make the timer work on multiple task

--- src/App.js
+++ src/App.js
@@ -1,28 +1,81 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const initialTasks = [
+  {
+    id: 1,
+    title: 'add new feature',
+    start: 0,
+    time: 0,
+    active: false
+  },
+  {
+    id: 2,
+    title: 'fix some bugs',
+    start: 0,
+    time: 0,
+    active: false
+  },
+  {
+    id: 3,
+    title: 'make some coffee',
+    start: 0,
+    time: 0,
+    active: false
+  }
+];
+
 function App() {
-  const [seconds, setSeconds] = useState(0);
-  const [active, setActive] = useState(false);
+  const [tasks, setTasks] = useState(initialTasks);
+  const [currentTask, setCurrentTask] = useState({});
 
   const pad = val => val > 9 ? val : '0' + val;
 
   useEffect(() => {
     let interval = null;
 
-    if (active) {
-      interval = setInterval(() => setSeconds(seconds => seconds + 1), 1000);
+    console.log(currentTask[0]);
+
+    if (currentTask[0]) {
+      interval = setInterval(() => setTasks(tasks.map(task => {
+        if (task.id === currentTask[0].id) {
+          return {
+            ...task,
+            active: true,
+            time: task.time + 1
+          };
+        }
+        return task;
+      })), 1000);
     }
 
     return () => clearInterval(interval);
-  }, [active]);
+  }, [tasks, currentTask]);
+
+  const startTimer = (taskID) => {
+    console.log('start timer button clicked...');
+
+    setCurrentTask(tasks.map(task => {
+      if (task.id === taskID) {
+        return {
+          ...task,
+          active: true
+        };
+      }
+      return task;
+    }).filter(task => task.id === taskID));
+  };  
 
   return (
     <div className="app">
       <h1>Taymer</h1>
-      <div className="task-item">
-        <p className="task-title">Drink Coffee <span>{pad(Math.floor(seconds / 60 % 60))}m:{pad(seconds % 60)}s</span></p>
-        <button onClick={() => setActive(!active)}>start</button>
+      <div className="task-list">
+        {tasks.map(task => (
+          <div key={task.id} className="task-item">
+            <p className="task-title">{task.title} <span>{pad(Math.floor(task.time / 60 % 60))}m:{pad(task.time % 60)}s</span></p>
+            <button onClick={() => startTimer(task.id)}>start</button>
+          </div>
+        ))}
       </div>
     </div>
   );
